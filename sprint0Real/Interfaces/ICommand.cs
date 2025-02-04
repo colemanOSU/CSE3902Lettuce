af611b9f80a14698438819b7f786053e87b179c5
Andie Stewart <stewart.2200@buckeyemail.osu.edu>
2025-02-04 09:35:16 -0500

    Added IBlock interface and Block content, organized Interrfaces into folder

---
+++ sprint0Real/Interfaces/ICommand.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace sprint0Real.Interfaces
+{
+    public interface ICommand
+    {
+        void Execute();
+    }
+}
