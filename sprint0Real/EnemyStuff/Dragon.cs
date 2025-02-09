7cf96e324ecf8ce893e3d3f4020b231427acfc46
Lettuce <allen.hi.com@gmail.com>
2025-02-09 01:37:57 -0500

    Polishing up dragon

--- sprint0Real/EnemyStuff/Dragon.cs
+++ sprint0Real/EnemyStuff/Dragon.cs
@@ -11,14 +11,19 @@ namespace sprint0Real.EnemyStuff
     public class Dragon
     {
         private DragonStateMachine stateMachine;
+        private DragonBehavior behavior;
+        
+        public GameTime time;
         public ISprite mySprite;
         public Vector2 location;
         public int speed = 10;
         public int health = 10;
 
-        public Dragon()
+        public Dragon(GameTime gameTime)
         {
+            time = gameTime;
             stateMachine = new DragonStateMachine(this);
+            behavior = new DragonBehavior(this); 
             mySprite = EnemySpriteFactory.Instance.CreateDragonEnemySprite();
         }
 
@@ -37,10 +42,16 @@ namespace sprint0Real.EnemyStuff
             stateMachine.Attack();
         }
 
-        public void Update()
+        public void Update(GameTime time)
         {
-            mySprite.Update(Game1.Instance._spriteBatch, location);
+            // Updates the location
+            stateMachine.Update();
+            // Moves onto the next frame in animation
+            mySprite.Update(Game1.Instance._spriteBatch, Game1.Instance.linkSheet);
+            // Draws the new frame
             mySprite.Draw(Game1.Instance._spriteBatch, location);
+            // Updates the state based on external states
+            behavior.Update(time);
         }
     }
 }
---
+++ sprint0Real/EnemyStuff/DragonBehavior.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace sprint0Real.EnemyStuff
+{
+    
+    public class DragonBehavior
+    {
+        private Dragon myDragon;
+        private float jukeDelay = 0f;
+        private float attackDelay = 5f;
+        private float jukeTimer = 0f;
+        private float attackTimer = 0f;
+        private Random random = new Random();
+
+        public DragonBehavior(Dragon dragon) {
+            myDragon = dragon;
+        } 
+
+        public void Update(GameTime time)
+        {
+            jukeTimer += (float) time.ElapsedGameTime.TotalSeconds;
+            attackTimer += (float) time.ElapsedGameTime.TotalSeconds;
+            
+            if (jukeDelay <= jukeTimer)
+            {
+                jukeTimer = 0;
+                jukeTimer = (float)(random.NextDouble() * 0.5);
+                myDragon.ChangeDirection();
+            }
+
+            // Gotta make the dragon doesn't juke it's way off screen
+            if (myDragon.location.X >= Game1.Instance._graphics.PreferredBackBufferWidth)
+            {
+                jukeTimer = 0;
+                myDragon.ChangeDirection();
+            }
+
+            if (attackDelay <= attackTimer)
+            {
+                attackTimer = 0;
+                myDragon.Attack();
+            }
+        }
+    }
+}
