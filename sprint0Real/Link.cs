fb214d1e8a4bf7d58b86de09f5e2227c20dd6a44
DaffPunk <numpadburner@gmail.com>
2025-02-10 10:43:01 -0500

    N/A

--- sprint0Real/Link.cs
+++ sprint0Real/Link.cs
@@ -34,7 +34,7 @@ public class Link : ILink
     public Link()
 	{
         sourceRectangle = new Rectangle(1, 11, 16, 16);
-        destinationRectangle = new Rectangle(200, 200, 50, 50);
+        destinationRectangle = new Rectangle(200, 200, 16, 16);
         canMove = true;
         canAttack = true;
         Facing = Direction.Right;
