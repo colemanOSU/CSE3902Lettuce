ff00f0d048f75dd2ab05d97b47459bcb8dad59f0
APURI12 <84340739+APURI12@users.noreply.github.com>
2025-02-10 10:18:45 -0500

    Added Links Animaition for picking up an item. May change it to specific items and create more classes for each item.

---
+++ sprint0Real/Interfaces/IItem.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+
+namespace sprint0Real.Interfaces
+{
+    public interface IItem
+    {
+        public void Update(SpriteBatch spriteBatch, Texture2D marioSheet);
+
+    }
+}
