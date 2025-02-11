db08d99b097057d271b780bb4a047c5cb78e3555
APURI12 <84340739+APURI12@users.noreply.github.com>
2025-02-10 22:07:58 -0500

    I added all the item sprites

---
+++ sprint0Real/ItemSprites/WhiteSwordSprite.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using sprint0Real.Interfaces;
+
+namespace sprint0Real.LinkSprites
+{
+    internal class WhiteSwordSprite : IItem
+    {
+        private Rectangle sourceRectangle = new(45, 154, 16, 16);
+        private Rectangle destinationRectangle;
+
+        private Texture2D _texture;
+        private Game1 myGame;
+        private int frameCount = 0;
+
+        public WhiteSwordSprite(Texture2D texture, Game1 game)
+        {
+            _texture = texture;
+            myGame = game;
+            destinationRectangle = new Rectangle(game.Link.GetLocation().X + 11, game.Link.GetLocation().Y + 1, 16, 16);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_texture, destinationRectangle, sourceRectangle, Color.White);
+        }
+
+        public void Update(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+
+        }
+
+        public void Update(SpriteBatch spriteBatch, Texture2D marioSheet)
+        {
+        }
+    }
+}
