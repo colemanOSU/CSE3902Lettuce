e883accb1c353929243b72939d2eb143e8d281d1
JeNNyder <jennyding0227@gmail.com>
2025-02-15 12:54:38 -0500

    boomerang

--- sprint0Real/Items/ItemSprites/BlueBoomerangSprite.cs
+++ sprint0Real/Items/ItemSprites/BlueBoomerangSprite.cs
@@ -1,34 +1,76 @@
 ﻿using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework;
 using sprint0Real.Interfaces;
+using sprint0Real.Commands;
+using System;
 
 namespace sprint0Real.Items.ItemSprites
 {
     internal class BlueBoomerangSprite : ILinkSprite
     {
-        private Rectangle sourceRectangle = new(91, 185, 8, 16);
+        private Rectangle sourceRectangle;
         private Rectangle destinationRectangle;
-
         private Texture2D _texture;
         private Game1 myGame;
-        private int frameCount = 0;
+        private Vector2 startPosition;
+        private Vector2 velocity;
+        private Vector2 _position;
+        private int _currentFrame;
+        private double _timer;
+        private bool isReturning = false;
+        private float travelDistance = 300f;
+        private float speed = 200f;
+        private float _frameSpeed = 0.2f;
+        private int _frameCount = 3;
 
         public BlueBoomerangSprite(Texture2D texture, Game1 game)
         {
             _texture = texture;
             myGame = game;
-            destinationRectangle = new Rectangle(game.Link.GetLocation().X + 11, game.Link.GetLocation().Y + 1, 16, 16);
+            startPosition = new(game.Link.GetLocation().X, game.Link.GetLocation().Y);
+            _position = startPosition;
+            SetVelocity(game.Link.GetFacing());
+        }
+
+        private void SetVelocity(Link.Direction direction)
+        {
+            velocity = direction switch
+            {
+                Link.Direction.Right => new Vector2(speed, 0),
+                Link.Direction.Left => new Vector2(-speed, 0),
+                Link.Direction.Up => new Vector2(0, -speed),
+                Link.Direction.Down => new Vector2(0, speed),
+                _ => Vector2.Zero,
+            };
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
+            sourceRectangle = new Rectangle(91 + 9 * _currentFrame, 185, 8, 16);
+            destinationRectangle = new Rectangle((int)_position.X, (int)_position.Y, 8 * 3, 16 * 3);
             spriteBatch.Draw(_texture, destinationRectangle, sourceRectangle, Color.White);
         }
 
         public void Update(GameTime gameTime, SpriteBatch spriteBatch)
         {
+            _timer += gameTime.ElapsedGameTime.TotalSeconds;
+            _position += velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
 
+            if (!isReturning && Vector2.Distance(_position, startPosition) >= travelDistance)
+            {
+                isReturning = true;
+                velocity *= -1;
+            }
+            else if (isReturning && Vector2.Distance(_position, startPosition) < 5f)
+            {
+                myGame.weaponItems = new NullSprite(_texture, myGame);
+            }
+
+            if (_timer > _frameSpeed)
+            {
+                _currentFrame = (_currentFrame + 1) % _frameCount;
+                _timer -= _frameSpeed;
+            }
         }
-
     }
 }
