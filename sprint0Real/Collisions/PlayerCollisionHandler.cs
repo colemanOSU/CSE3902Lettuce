2d5b57eec9cccb711c29c4dcaa7dbcea63cf29a6
APURI12 <84340739+APURI12@users.noreply.github.com>
2025-02-24 10:49:37 -0500

    Created PlayerCollisionHandler.cs and a skeleton of collision responses.

---
+++ sprint0Real/Collisions/PlayerCollisionHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using sprint0Real.BlockSprites;
+using sprint0Real.Interfaces;
+
+namespace sprint0Real.Collisions
+{
+    internal class PlayerCollisionHandler
+    {
+        /*
+        void HandleBlockCollision(IBlock block, ICollision side)
+        {
+            switch(side){
+                case Left:
+
+                case Right:
+
+                case Top:
+
+                case Bottom:
+
+                case None:
+
+                defaut:
+
+
+            }
+        }
+        void HandleEnemyCollision(IEnemy enemy, ICollision side)
+        {
+
+        }
+        */
+    }
+}
