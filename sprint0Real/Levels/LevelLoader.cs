82cc5ea03f8f200c859bc463c9ba9d57ce33482b
Lettuce <allen.hi.com@gmail.com>
2025-02-28 20:53:06 -0500

    Added some interfaces and wrote some preliminary code for level loader

--- sprint0Real/Levels/LevelLoader.cs
+++ sprint0Real/Levels/LevelLoader.cs
@@ -1,17 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using Microsoft.Xna.Framework;
+using sprint0Real.Interfaces;
+using static System.Net.WebRequestMethods;
 
 namespace sprint0Real.Levels
 {
     public class LevelLoader
     {
         Dictionary<String, EnemyPage> Maps = new Dictionary<String, EnemyPage>();
-        // for each xml document{add all the stuff to the gameobjects list, making sure to cast each thing
-        // with its proper interface.}
+        private static LevelLoader instance = new LevelLoader();
+        public static LevelLoader Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
 
-        // Will this be a singleton? - sure why not. Shouldn't be too hard to change later. 
+        void LoadLevels()
+        {
+            foreach (String fileName in Directory.GetFiles("C: \\Users\\icebe\\Source\\Repos\\CSE3902Lettuce\\sprint0Real\\Levels\\Level Files\\"))
+            {
+                EnemyPage newMap = new EnemyPage();
+                XmlDocument xml = new XmlDocument();
+                xml.LoadXml(fileName);
+                foreach(XmlElement Object in xml.SelectNodes("Objects"))
+                {
+                    Type type = Type.GetType(Object.GetAttribute("Type"));
+                    int x = Int32.Parse(Object.GetAttribute("x"));
+                    int y = Int32.Parse(Object.GetAttribute("y"));
+                    newMap.Stage((IGameObject)Activator.CreateInstance(type, new Vector2(x, y)));
+                }
+                foreach(XmlElement MapHitBox in xml.SelectNodes("MapHitBoxes"))
+                {
+                    Type type = Type.GetType(MapHitBox.GetAttribute("Type"));
+                    int x = Int32.Parse(MapHitBox.GetAttribute("x"));
+                    int y = Int32.Parse(MapHitBox.GetAttribute("y"));
+                    int width = Int32.Parse(MapHitBox.GetAttribute("width"));
+                    int height = Int32.Parse(MapHitBox.GetAttribute("height"));
+                    newMap.AddCollisionBox((ICollisionBoxes)Activator.CreateInstance(type, new Rectangle(x, y, width, height)));
+                }
+
+                foreach(XmlElement Neighbor in xml.SelectNodes("Neighbors"))
+                {
+                    newMap.AddNeighbor(Neighbor.GetAttribute("Side"), Neighbor.GetAttribute("Name"));
+                }
+                Maps.Add(xml.GetElementById("Name").GetAttribute("Name"), newMap);
+            }
+            CurrentMap.Instance.SetMap(Maps["Entrance"]);
+        }
     }
 }
