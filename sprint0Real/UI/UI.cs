a78b9c802cf15b9f0da92b9ddc5babe19d747d84
colemanOSU <coleman.1217@osu.edu>
2025-03-11 17:46:30 -0400

    Implements functional healthbar UI

--- sprint0Real/UI/UI.cs
+++ sprint0Real/UI/UI.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using sprint0Real;
 using sprint0Real.Interfaces;
+using System.Diagnostics;
 
 
 public class UI : IUI
@@ -12,6 +13,12 @@ public class UI : IUI
     private int Scale = Game1.RENDERSCALE;
     private int UIXCoord;
     private int UIYCoord;
+    private Rectangle[] RupeeDigits;
+    private Rectangle[] KeyDigits;
+    private Rectangle[] BombDigits;
+    private Rectangle[] HealthBar;
+
+    private int MaxHealth;
     public UI(Texture2D uITexture)
     {
         UIXCoord = Game1.SCREENMIDX - (128 * Scale);
@@ -20,8 +27,13 @@ public class UI : IUI
         SourceRectangle = new Rectangle(258, 11, 256, 56);
 
         UITexture = uITexture;
-        
 
+        RupeeDigits = UIHelper.CounterHelper(00);
+        KeyDigits = UIHelper.CounterHelper(00);
+        BombDigits = UIHelper.CounterHelper(00);
+
+        MaxHealth = 10;
+        HealthBar = UIHelper.HealthbarHelper(MaxHealth, MaxHealth);
 
     }
 
@@ -29,26 +41,37 @@ public class UI : IUI
     {
         spriteBatch.Draw(UITexture, DestinationRectangle, SourceRectangle, Color.White);
 
-        //Amount set to arbitrary number for testing purposes
-        Rectangle[] RupeeDigits = CounterHelper.Helper(05);
+        
         //Rupee Counter Ones Digit and Ten Digit
         spriteBatch.Draw(UITexture, new Rectangle(UIXCoord + 104 * Scale, UIYCoord + 16 * Scale, 8 * Scale, 8 * Scale), RupeeDigits[1], Color.White);
         spriteBatch.Draw(UITexture, new Rectangle(UIXCoord + 112 * Scale, UIYCoord + 16 * Scale, 8 * Scale, 8 * Scale), RupeeDigits[0], Color.White);
 
-        Rectangle[] KeyDigits =  CounterHelper.Helper(99);
+        
         //Rupee Counter Ones Digit and Ten Digit
         spriteBatch.Draw(UITexture, new Rectangle(UIXCoord + 104 * Scale, UIYCoord + 32 * Scale, 8 * Scale, 8 * Scale), KeyDigits[1], Color.White);
         spriteBatch.Draw(UITexture, new Rectangle(UIXCoord + 112 * Scale, UIYCoord + 32 * Scale, 8 * Scale, 8 * Scale), KeyDigits[0], Color.White);
 
-        Rectangle[] BombDigits = CounterHelper.Helper(03);
+        
         //Rupee Counter Ones Digit and Ten Digit
         spriteBatch.Draw(UITexture, new Rectangle(UIXCoord + 104 * Scale, UIYCoord + 40 * Scale, 8 * Scale, 8 * Scale), BombDigits[1], Color.White);
         spriteBatch.Draw(UITexture, new Rectangle(UIXCoord + 112 * Scale, UIYCoord + 40 * Scale, 8 * Scale, 8 * Scale), BombDigits[0], Color.White);
+
+        //Health Bar
+        for (int i = 0; i < MaxHealth / 2; i++)
+        {
+            spriteBatch.Draw(UITexture, new Rectangle(UIXCoord + 176 * Scale + (i * 8 * Scale), UIYCoord + 32 * Scale, 8 * Scale, 8 * Scale), HealthBar[i], Color.White);
+        }
     }
 
     public void Update(GameTime gametime, SpriteBatch spriteBatch)
     {
-    
+        //Amount set to arbitrary number for testing purposes
+        RupeeDigits = UIHelper.CounterHelper(05);
+        KeyDigits = UIHelper.CounterHelper(99);
+        BombDigits = UIHelper.CounterHelper(03);
+
+        //Health Set to Arbitrary number for testing purposes
+        HealthBar = UIHelper.HealthbarHelper(MaxHealth, 4);
     }
 }
 
---
+++ sprint0Real/UI/UIHelper.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using sprint0Real;
+using sprint0Real.Interfaces;
+using System;
+using System.Diagnostics;
+
+public static class UIHelper
+{
+	//Function takes in a number from 0 to 99 and returns the source
+	//rectangles for each digit on the sprite sheet as an array.
+	//All inputs greater than 99 are treated as 99.
+	//Inputs must be positive.
+	public static Rectangle[] CounterHelper(int input)
+	{
+		
+		if (input > 99) input = 99;
+
+		//Get each digit
+		int OneDigit = input % 10;
+		int TenDigit = input / 10;
+
+		//Set each digit's source to the "0" sprite by default
+		Rectangle OneDigitRec = new Rectangle(528, 117, 8, 8);
+		Rectangle TenDigitRec = new Rectangle(528, 117, 8, 8);
+
+		OneDigitRec.Offset(9 * OneDigit, 0);
+        TenDigitRec.Offset(9 * TenDigit, 0);
+
+        Rectangle[] AnswerArray = {OneDigitRec, TenDigitRec};
+		return AnswerArray;
+	}
+
+	//Takes the current health and maximum health of Link as inputs
+	//And returns the source rectangles for the healthbar sprite as
+	//an array of length 8.
+
+	//Health is measured in half hearts (so 2 hearts = 4 health)
+	//Assumes maximum possible maximum health of 8 hearts.
+
+	//Max Health MUST ALWAYS BE an even Number!
+	public static Rectangle[] HealthbarHelper(int MaxHealth, int CurrentHealth)
+	{
+		if (MaxHealth % 2 != 0)
+		{
+			throw new ArgumentException("Max Health must be an even number!");
+        }
+
+		Rectangle[] ReturnArray = new Rectangle[MaxHealth / 2];
+		
+
+		
+
+		for (int i = 0; i < MaxHealth / 2; i++)
+		{
+			if (CurrentHealth >= 2)
+			{
+				ReturnArray[i] = new Rectangle(645, 117, 8, 8);
+			}
+			else if (CurrentHealth == 1) { 
+				ReturnArray[i] = new Rectangle(636, 117, 8, 8); 
+			}
+			else ReturnArray[i] = new Rectangle(627, 117, 8, 8);
+
+			CurrentHealth--;
+            CurrentHealth--;
+
+
+        }
+
+		return ReturnArray;
+	}
+}
