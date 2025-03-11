3fcf9ba5f959ed1b4852b4e49e478f10d825879a
colemanOSU <coleman.1217@osu.edu>
2025-03-11 16:56:09 -0400

    Implements UI with working counters for item amounts

--- sprint0Real/UI/UI.cs
+++ sprint0Real/UI/UI.cs
@@ -10,17 +10,40 @@ public class UI : IUI
     private Rectangle DestinationRectangle;
     private Rectangle SourceRectangle;
     private int Scale = Game1.RENDERSCALE;
+    private int UIXCoord;
+    private int UIYCoord;
     public UI(Texture2D uITexture)
     {
-        UITexture = uITexture;
-        DestinationRectangle = new Rectangle(Game1.SCREENMIDX - (128 * Scale), Game1.SCREENMIDY - (88 * Scale) - 56 * Scale, 256 * Scale, 56 * Scale);
+        UIXCoord = Game1.SCREENMIDX - (128 * Scale);
+        UIYCoord = Game1.SCREENMIDY - (88 * Scale) - 56 * Scale;
+        DestinationRectangle = new Rectangle(UIXCoord, UIYCoord, 256 * Scale, 56 * Scale);
         SourceRectangle = new Rectangle(258, 11, 256, 56);
+
+        UITexture = uITexture;
         
+
+
     }
 
     public void Draw(SpriteBatch spriteBatch)
     {
         spriteBatch.Draw(UITexture, DestinationRectangle, SourceRectangle, Color.White);
+
+        //Amount set to arbitrary number for testing purposes
+        Rectangle[] RupeeDigits = CounterHelper.Helper(05);
+        //Rupee Counter Ones Digit and Ten Digit
+        spriteBatch.Draw(UITexture, new Rectangle(UIXCoord + 104 * Scale, UIYCoord + 16 * Scale, 8 * Scale, 8 * Scale), RupeeDigits[1], Color.White);
+        spriteBatch.Draw(UITexture, new Rectangle(UIXCoord + 112 * Scale, UIYCoord + 16 * Scale, 8 * Scale, 8 * Scale), RupeeDigits[0], Color.White);
+
+        Rectangle[] KeyDigits =  CounterHelper.Helper(99);
+        //Rupee Counter Ones Digit and Ten Digit
+        spriteBatch.Draw(UITexture, new Rectangle(UIXCoord + 104 * Scale, UIYCoord + 32 * Scale, 8 * Scale, 8 * Scale), KeyDigits[1], Color.White);
+        spriteBatch.Draw(UITexture, new Rectangle(UIXCoord + 112 * Scale, UIYCoord + 32 * Scale, 8 * Scale, 8 * Scale), KeyDigits[0], Color.White);
+
+        Rectangle[] BombDigits = CounterHelper.Helper(03);
+        //Rupee Counter Ones Digit and Ten Digit
+        spriteBatch.Draw(UITexture, new Rectangle(UIXCoord + 104 * Scale, UIYCoord + 40 * Scale, 8 * Scale, 8 * Scale), BombDigits[1], Color.White);
+        spriteBatch.Draw(UITexture, new Rectangle(UIXCoord + 112 * Scale, UIYCoord + 40 * Scale, 8 * Scale, 8 * Scale), BombDigits[0], Color.White);
     }
 
     public void Update(GameTime gametime, SpriteBatch spriteBatch)
@@ -28,3 +51,5 @@ public class UI : IUI
     
     }
 }
+
+
