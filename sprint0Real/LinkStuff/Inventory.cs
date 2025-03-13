ab85b650e590cbd4aff4844c083cae2316498707
colemanOSU <coleman.1217@osu.edu>
2025-03-13 15:23:24 -0400

    Adds inventory class to keep track of obtained and currently equipped items

---
+++ sprint0Real/LinkStuff/Inventory.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sprint0Real.LinkStuff
+{
+    class Inventory
+    {
+        //That is the question:
+        //Whether tis nobler in the mind to suffer the slings and arrows of outrageous fortune,
+        //Or to take arms against a sea of troubles, And by opposing end them
+
+        private Dictionary<Items, Boolean> InventoryList;
+        public Items? CurrentItem { get; set; }
+        public enum Items
+        {
+            Boomerang,
+            M_Boomerang,
+            Bomb,
+            Bow,
+            Candle,
+            Flute,
+            Meat,
+            Potion,
+            Staff
+        }
+
+        public Inventory()
+        {
+            foreach (Items item in Enum.GetValues(typeof(Items)))
+            {
+                InventoryList.Add(item, false);
+            }
+            CurrentItem = null;
+        }
+
+        public void ObtainItem(Items item)
+        {
+            InventoryList.Remove(item);
+            InventoryList.Add(item, true);
+        }
+
+        public bool HasItem(Items item)
+        {
+            return InventoryList.GetValueOrDefault(item);
+        }
+        
+        
+    }
+}
