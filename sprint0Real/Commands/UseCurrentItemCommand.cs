a6f044c4ab9d3f98a0bcadddc159365a2406ca64
colemanOSU <coleman.1217@osu.edu>
2025-03-23 16:57:05 -0400

    Adds Blank UseCurrentItemsCommand

---
+++ sprint0Real/Commands/UseCurrentItemCommand.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using sprint0Real.Collisions;
+using sprint0Real.Interfaces;
+using sprint0Real.ItemUseSprites;
+using sprint0Real.Levels;
+using sprint0Real.LinkStuff.LinkSprites;
+
+namespace sprint0Real.Commands
+{
+    public class UseCurrentItemCommand : ICommand
+    {
+        private Game1 myGame;
+        public UseCurrentItemCommand(Game1 game)
+        {
+            myGame = game;
+
+            
+        }
+        //TODO
+        public void Execute()
+        {
+            if (myGame.Link.CanAttack())
+            {
+                myGame.Link.SetCanAttack(false);
+                myGame.Link.SetCanMove(false);
+                
+            }
+
+        }
+    }
+}
