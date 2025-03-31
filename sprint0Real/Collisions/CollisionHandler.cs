ee1f725189e42902246665e1a72f6558764865c2
Andie Stewart <stewart.2200@buckeyemail.osu.edu>
2025-03-31 19:36:56 -0400

    fixing merge issues

--- sprint0Real/Collisions/CollisionHandler.cs
+++ sprint0Real/Collisions/CollisionHandler.cs
@@ -29,15 +29,14 @@ namespace sprint0Real.Collisions
         public CollisionHandler(Game1 game)
 {
     this.game = game;
-    collisionCommands = new Dictionary<(String, String), ICollisionCommand2>();
-    LoadCommands();
+            collisionCommands = new Dictionary<(String, String), ICollisionCommand2>();
 }
 public void LoadCommands()
 {
 
     collisionCommands.Add(("Link", "enemy"),  new LinkEnemyCommand());
     collisionCommands.Add(("Link", "Border"), new LinkBorderCommand());
-    collisionCommands.Add(("Link", "RoomTransitionBox"), new RoomTransitionCommand());
+    collisionCommands.Add(("Link", "RoomTransitionBox"), new RoomTransitionCommand(game.collisionDetection));
     collisionCommands.Add(("Enemy", "LinkWeapon"), new DamageEnemyCollisionCommand());
 
             collisionCommands.Add(("Link", "FireBall"), new LinkEnemyCommand());
--- sprint0Real/Items/ItemSprites/BombSprite.cs
+++ sprint0Real/Items/ItemSprites/BombSprite.cs
@@ -40,7 +40,7 @@ namespace sprint0Real.Items.ItemSprites
             SetPosition(game.Link.GetFacing());
             destinationRectangle = new Rectangle((int)_position.X, (int)_position.Y, isDelaying ? 8 * 3 : 16 * 3, 16 * 3);
             //bombDrop = SoundEffectFactory.Instance.GetWeaponSoundEffect(ItemStateMachine.Item.Bomb);
-            bombExplode = SoundEffectFactory.Instance.GetBombExplode();
+            //bombExplode = SoundEffectFactory.Instance.GetBombExplode();
         }
         public Rectangle Rect
         {
