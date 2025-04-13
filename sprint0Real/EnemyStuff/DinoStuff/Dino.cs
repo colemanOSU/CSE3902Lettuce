0dead2db65f9e5a998b63696b5b021cf13684652
Lettuce <allen.hi.com@gmail.com>
2025-04-12 23:25:49 -0400

    Added files for dino

---
+++ sprint0Real/EnemyStuff/DinoStuff/Dino.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using sprint0Real.Interfaces;
+
+namespace sprint0Real.EnemyStuff.DinoStuff
+{
+    public class Dino : IEnemy
+    {
+        public Vector2 location { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public Rectangle Rect => throw new NotImplementedException();
+
+        public void ChangeDirection()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TakeDamage(int damage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ sprint0Real/EnemyStuff/DinoStuff/DinoWalkingDownSprite.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using sprint0Real.Interfaces;
+
+namespace sprint0Real.EnemyStuff.DinoStuff
+{
+    public class DinoWalkingDownSprite : ISprite2
+    {
+        private Texture2D sprites;
+        private int currentFrame;
+        private int totalFrames;
+
+        public DinoSprite(Texture2D spriteSheet, SpriteBatch spriteBatch)
+        {
+            sprites = spriteSheet;
+            totalFrames = 2;
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Vector2 location)
+        {
+            Rectangle sourceRectangle;
+            Rectangle destinationRectangle;
+            Vector2 org;
+            org.X = 0;
+            org.Y = 0;
+
+            if (currentFrame == 0)
+            {
+                sourceRectangle = new Rectangle(1, 59, 16, 16);
+                destinationRectangle = new Rectangle((int)location.X,
+                (int)location.Y, 16 * Game1.RENDERSCALE, 16 * Game1.RENDERSCALE);
+            }
+            else
+            {
+                sourceRectangle = new Rectangle(159, 78, 16, 16);
+                destinationRectangle = new Rectangle((int)location.X,
+                (int)location.Y, 16 * Game1.RENDERSCALE, 16 * Game1.RENDERSCALE);
+            }
+            spriteBatch.Draw(sprites, destinationRectangle, sourceRectangle, Color.White);
+        }
+        public void Update()
+        {
+            currentFrame = (currentFrame + 1) % totalFrames;
+        }
+    }
+}
