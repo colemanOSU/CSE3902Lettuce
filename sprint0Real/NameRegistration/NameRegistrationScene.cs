c729d3a71318374aa181f0d23e0df78018d1bd42
Andie Stewart <stewart.2200@buckeyemail.osu.edu>
2025-04-14 10:55:32 -0400

    name registration

---
+++ sprint0Real/NameRegistration/NameRegistrationScene.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace sprint0Real.NameRegistration
+{
+    public class NameRegistrationScene
+    {
+
+        private Texture2D spriteSheet;
+        private Dictionary<char, Rectangle> charMap;
+        private StringBuilder playerName;
+        private int cursorX = 0, cursorY = 0;
+        private const int maxNameLength = 8;
+        private Vector2 gridStart = new Vector2(40, 100); // Adjust as needed
+        private int charSpacing = 16;
+
+
+        private string[,] charGrid =
+        {
+            { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J" },
+            { "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T" },
+            { "U", "V", "W", "X", "Y", "Z", ".", ",", "!", "%" },
+            { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" },
+        };
+
+        private string[] options = { "REGISTER", "END" };
+        private int optionIndex = -1; // -1 = not selected
+
+        public NameRegistrationScene(Texture2D spriteSheet)
+        {
+            this.spriteSheet = spriteSheet;
+            playerName = new StringBuilder();
+
+            charMap['A'] = new Rectangle(303, 126, 8, 8);
+        }
+    }
+}
--- sprint0Real/TreasureItemStuff/TreasureItemSprites/FairyStateMachine.cs
+++ sprint0Real/TreasureItemStuff/TreasureItemSprites/FairyStateMachine.cs
@@ -11,7 +11,7 @@ namespace sprint0Real.TreasureItemStuff.TreasureItemSprites
 {
     public class FairyStateMachine : IStateMachine
     {
-        private enum FairyStates { LeftDiagonal, RightDiagonal, Up, Down }
+        private enum FairyStates { LeftDiagonalDown, RightDiagonalDown, RightDiagonalUp, LeftDiagonalUp, Up, Down }
         private FairyStates currentState = FairyStates.Up;
         private Fairy myFairy;
         private Random random = new Random();
@@ -22,7 +22,7 @@ namespace sprint0Real.TreasureItemStuff.TreasureItemSprites
         }
         public void ChangeDirection()
         {
-            int nextDirection = random.Next(0, 4);
+            int nextDirection = random.Next(0, 6);
             switch (nextDirection)
             {
                 case 0:
@@ -32,10 +32,16 @@ namespace sprint0Real.TreasureItemStuff.TreasureItemSprites
                     currentState = FairyStates.Down;
                     break;
                 case 2:
-                    currentState = FairyStates.LeftDiagonal;
+                    currentState = FairyStates.LeftDiagonalDown;
                     break;
                 case 3:
-                    currentState = FairyStates.RightDiagonal;
+                    currentState = FairyStates.RightDiagonalDown;
+                    break;
+                case 4:
+                    currentState = FairyStates.LeftDiagonalUp;
+                    break;
+                case 5:
+                    currentState = FairyStates.RightDiagonalUp;
                     break;
             }
         }
@@ -43,11 +49,17 @@ namespace sprint0Real.TreasureItemStuff.TreasureItemSprites
         {
             switch (currentState)
             {
-                case FairyStates.RightDiagonal:
+                case FairyStates.RightDiagonalDown:
                     myFairy.location = new Vector2(myFairy.location.X + myFairy.speed, myFairy.location.Y + myFairy.speed);
                     break;
-                case FairyStates.LeftDiagonal:
+                case FairyStates.LeftDiagonalDown:
                     myFairy.location = new Vector2(myFairy.location.X - myFairy.speed, myFairy.location.Y + myFairy.speed);
+                    break;
+                case FairyStates.LeftDiagonalUp:
+                    myFairy.location = new Vector2(myFairy.location.X - myFairy.speed, myFairy.location.Y - myFairy.speed);
+                    break;
+                case FairyStates.RightDiagonalUp:
+                    myFairy.location = new Vector2(myFairy.location.X + myFairy.speed, myFairy.location.Y - myFairy.speed);
                     break;
                 case FairyStates.Up:
                     myFairy.location = new Vector2(myFairy.location.X, myFairy.location.Y - myFairy.speed);
