bde8fc550129e7161c9f0d499f2c55f44ac26a4e
JeNNyder <jennyding0227@gmail.com>
2025-04-17 16:38:47 -0400

    temp

--- sprint0Real/WolfLink/Wolf.cs
+++ sprint0Real/WolfLink/Wolf.cs
@@ -6,11 +6,22 @@ using System.Threading.Tasks;
 
 namespace sprint0Real.WolfLink
 {
-    private WolfStateMachine wolfstatemachine;
     public class Wolf
     {
+        private WolfStateMachine wolfstatemachine;
+        public enum Direction
+        {
+            Up,
+            Down,
+            Left,
+            Right,
+            None
+        }
+        public Wolf()
+        {
+            wolfstatemachine = new WolfStateMachine();
+        }
         
     }
-    public class getWolfLocation() { 
-    }
+    
 }
